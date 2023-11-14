1de078ef3cfac3dd5c66dc9c1ae54296508830db
Richard Y. Hayashi <rhayashi@gruuw.com>
2023-11-14 22:52:11 +0900

    Finished Ep. 9

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,16 +6,32 @@ use Illuminate\Support\Facades\Route;
 use Barryvdh\Debugbar\Facades\Debugbar;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    GET
+    POST
+    PUT
+    PATCH
+    DELETE
+    OPTIONS
 */
 
 Route::resource('/blog', PostsController::class);
 
+// // GET
+// Route::get('/blog', [PostsController::class, 'index']);
+// Route::get('/blog/1', [PostsController::class, 'show']);
+// // POST
+// Route::get('/blog/create', [PostsController::class, 'create']);
+// Route::post('/blog', [PostsController::class, 'store']);
+// // PUT or PATCH
+// Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+// Route::patch('/blog/1', [PostsController::class, 'update']);
+// // DELETE
+// Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
 Route::get('/', HomeController::class);
+
+// Multiple HTTP verbs
+//Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+//Route::any('/blog', [PostsController::class, 'index']);
+
+//Route::view('/blog', 'blog.index', ['name' => 'Code with Dary']);
